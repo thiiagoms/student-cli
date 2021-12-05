27d066243c040f6be4682fe961897cc57648b1f3
thiiagoms <thiagom.devsec@gmail.com>
2021-12-05 13:44:10 -0300

    feat: add env vars with database connection

--- src/Infra/Persistence/Database.php
+++ src/Infra/Persistence/Database.php
@@ -13,22 +13,71 @@ use PDOException, PDO;
  */
 class Database
 {
-    /** 
-     * Default database path
+    /**
+     * Database host
      * 
      * @var string
-    */
-    private const DBPATH = __DIR__ . '/../../db.sqlite'; // change to mysql credentials
+     */
+    private string $dbHost;
+
+    /**
+     * Database port
+     * 
+     * @var string
+     */
+    private string $dbPort;
+
+    /**
+     * Database name
+     * 
+     * @var string
+     */
+    private string $dbName;
+
+    /**
+     * Database user
+     * 
+     * @var string
+     */
+    private string $dbUser;
+
+    /**
+     * User password
+     * 
+     * @var string
+     */
+    private string $dbPass;
+
+    /** 
+     * Database connection
+     * 
+     * @var PDO
+     */
+    private PDO $con;
+
+    public function __construct()
+    {
+        (new Env())->load();
+
+        $this->dbHost = $_ENV['DATABASE_HOST'];
+        $this->dbPort = $_ENV['DATABASE_PORT'];
+        $this->dbName = $_ENV['DATABASE_NAME'];
+        $this->dbUser = $_ENV['DATABASE_USER'];
+        $this->dbPass = $_ENV['DATABASE_PASS'];
+    }
 
     /**
      * Create database connection
      * 
      * @return PDO
      */
-    public static function open(): PDO
+    public function open(): PDO
     {
         try {
-            return new PDO('sqlite: ' . self::DBPATH);
+            $this->con = new PDO('mysql:host=' . $this->dbHost . ';port=' . $this->dbPort . ';dbname=' . $this->dbName, $this->dbUser, $this->dbPass);
+            $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->con->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $this->con;
         } catch (PDOException $e) {
             echo "Code: {$e->getCode()}\n Message: {$e->getMessage()}";
         }
